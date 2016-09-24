a6779811c566297f78a613c6c4e9d14c0788878f
Peter Kona <ikona23@gmail.com>
2016-09-24 23:14:22 +1200

    callback

--- promise.js
+++ promise.js
@@ -1,43 +1,17 @@
-'use strict';
-var promiseCount = 0;
+function placeOrder(orderNumber) {
+  console.log('cusotmer order:', orderNumber);
 
-function testPromise() {
-    var thisPromiseCount = ++promiseCount;
-
-    var log = document.getElementById('log');
-    log.insertAdjacentHTML('beforeend', thisPromiseCount +
-        ') Started (<small>Sync code started</small>)<br/>');
-
-    // We make a new promise: we promise a numeric count of this promise, starting from 1 (after waiting 3s)
-    var p1 = new Promise(
-        // The resolver function is called with the ability to resolve or
-        // reject the promise
-        function(resolve, reject) {
-            log.insertAdjacentHTML('beforeend', thisPromiseCount +
-                ') Promise started (<small>Async code started</small>)<br/>');
-            // This is only an example to create asynchronism
-            window.setTimeout(
-                function() {
-                    // We fulfill the promise !
-                    resolve(thisPromiseCount);
-                }, Math.random() * 2000 + 1000);
-        }
-    );
-
-    // We define what to do when the promise is resolved/fulfilled with the then() call,
-    // and the catch() method defines what to do if the promise is rejected.
-    p1.then(
-        // Log the fulfillment value
-        function(val) {
-            log.insertAdjacentHTML('beforeend', val +
-                ') Promise fulfilled (<small>Async code terminated</small>)<br/>');
-        })
-    .catch(
-        // Log the rejection reason
-        function(reason) {
-            console.log('Handle rejected promise ('+reason+') here.');
-        });
-
-    log.insertAdjacentHTML('beforeend', thisPromiseCount +
-        ') Promise made (<small>Sync code terminated</small>)<br/>');
+  cookAndDeliveryFood(function(){
+    console.log("deiver food", orderNumber);
+  })
 }
+
+function cookAndDeliveryFood(callback){
+  setTimeout (callback, 5000)
+}
+
+placeOrder(1)
+placeOrder(2)
+placeOrder(3)
+placeOrder(4)
+placeOrder(5)
