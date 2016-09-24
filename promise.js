5b241a76ce67983097d07f6939cb3aa0e66f9d28
Peter Kona <ikona23@gmail.com>
2016-09-24 23:21:11 +1200

    objects

--- promise.js
+++ promise.js
@@ -1,17 +1,27 @@
-function placeOrder(orderNumber) {
-  console.log('cusotmer order:', orderNumber);
+// function placeOrder(orderNumber) {
+//   console.log('cusotmer order:', orderNumber);
+//
+//   cookAndDeliveryFood(function(){
+//     console.log("deiver food", orderNumber);
+//   })
+// }
+//
+// function cookAndDeliveryFood(callback){
+//   setTimeout (callback, 5000)
+// }
+//
+// placeOrder(1)
+// placeOrder(2)
+// placeOrder(3)
+// placeOrder(4)
+// placeOrder(5)
 
-  cookAndDeliveryFood(function(){
-    console.log("deiver food", orderNumber);
-  })
+var peter = {
+  favFood:"haluskay",
+  favMovie: "beats that shit"
 }
+  console.log(peter.favFood);
+var Person = peter
+Person.favFood = "salad"
 
-function cookAndDeliveryFood(callback){
-  setTimeout (callback, 5000)
-}
-
-placeOrder(1)
-placeOrder(2)
-placeOrder(3)
-placeOrder(4)
-placeOrder(5)
+console.log(peter.favFood);
