d1b24b7a24b49e6cfdfffdf4e6aaf0f7afa0c675
Peter Kona <ikona23@gmail.com>
2016-09-26 00:21:00 +1300

    adding form

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,16 @@ router.get('/login',function(req,res){
   res.render('login')
 })
 
+// Register Users
+router.post('/register', function(req,res){
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+  console.log(name);
+
+  req.checkBody('name','Name is required').notEmpty()
+})
+
 module.exports = router;
